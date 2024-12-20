9f085c12a754c3f36efca44fdad41b803a378105
kevinh81760 <kevinh81760@gmail.com>
2024-12-19 20:47:45 -0800

    initial commit

---
+++ server/src/index.mjs
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Running on Port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
