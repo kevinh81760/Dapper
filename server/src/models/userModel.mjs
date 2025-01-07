d2c694e169762e0dd62333391a92e9fd8ef1f158
kevinh81760 <kevinh81760@gmail.com>
2025-01-06 22:41:11 -1000

    environment setup

--- server/src/models/userModel.mjs
+++ server/src/models/userModel.mjs
@@ -1,41 +1,29 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcrptjs';
 
-const UserSchema = new mongoose.Schema
-({
-    username: 
-    {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    displayName: 
-    {
-        type: String,
-    },
-    password: 
-    {
-        type: String,
-        required: true,
-    },
-});
-
-UserSchema.pre('save', async function (next) 
+// Define Schema
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true},
+    password: { type: String, required: true }
+},
 {
-    if(!this.isModified('password')) 
-    {
+    timestamps: true 
+});
+    
+// Pre-save hook to hash passwords
+userSchema.pre('save', async function (next) {
+    if(!this.isModified('password')) {
         return next();
     }
-
-    try
-    {
+    try {
         const salt = await bcrypt.genSalt(10);
         this.password = await bcrypt.hash(this.password, salt);
         next();
     }
-    catch(err)
-    {
+    catch(err) {
         return next(err);
     }
 });
+
 
 export const User = mongoose.model('User', UserSchema);
--- server/src/server.mjs
+++ server/src/server.mjs
@@ -1,10 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.use(express.json());
+
+app.listen(PORT, (err) => {
+    if(err) {
+        console.error('Failed to start server: ${err.message}')
+        process.exit(1);
+    }
     console.log(`Running on Port ${PORT}`);
 });
 
